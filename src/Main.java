e7a48abf89853a80492eea701085161f2363e044
Phoenix <phoenixreid@mail.ru>
2024-05-23 17:45:18 +0300

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int replenishment = 13451;
+
+        if (replenishment >= 1000) {
+            System.out.println("Количество денег на счету: " + (score + replenishment + replenishment/100));
+            System.out.println("Количество бонусов: " + (replenishment/100));
+        }
+        else {
+            System.out.println("Количество денег на счету: " + (score + replenishment));
+        }
+    }
+}
